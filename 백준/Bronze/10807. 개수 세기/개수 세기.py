aa83464207d3b56a826f7c073d6a81fcb1448706
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-08 14:34:25 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,10 +1,4 @@
-N = input()
-a = input().split()
+n = int(input())
+arr = list(map(int, input().split()))
 v = int(input())
-sum = 0
-
-for i in a:
-    if int(i) == v:
-        sum += 1
-
-print(sum)
+print(arr.count(v))
