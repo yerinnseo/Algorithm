3e9ed1b8b41065fe8694099c0e50ed5204140170
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-08 10:42:54 +0900

    [Bronze V] Title: A+B - 7, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,7 +1,5 @@
 T = int(input())
-num = 0
 
 for i in range(1, T+1):
     a, b = map(int, input().split())
-    num += 1
-    print("Case #{0}:".format(num), a+b)
+    print("Case #{}: {}".format(i, a+b))
