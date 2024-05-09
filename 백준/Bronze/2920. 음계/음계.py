f0b6358d4c11e5e5f240e79924cb85510df18b5d
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-09 16:03:16 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,8 +1,8 @@
 a = list(map(int, input().split()))
 
-if a == [1, 2, 3, 4, 5, 6, 7, 8]:
+if a == sorted(a):
     print("ascending")
-elif a == [8, 7, 6, 5, 4, 3, 2, 1]:
+elif a == sorted(a, reverse = True):
     print("descending")
 else:
     print("mixed")
