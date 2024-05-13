a0daf46278ad083f8f97711c82fb4de543446b4a
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-13 18:43:26 +0900

    [Bronze II] Title: 분해합, Time: 1464 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(1, n+1):
+    nums = list(map(int, str(i)))
+    result = sum(nums) + i
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
