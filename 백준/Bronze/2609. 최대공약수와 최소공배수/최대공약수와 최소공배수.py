2a582889ef8922fea20b98641d0b1583b8908c90
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-14 16:32:58 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,18 +1,9 @@
+def gcd(a, b):
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
 a, b = map(int, input().split())
-l1 = []
-l2 = []
-
-for i in range(1, a+1):
-    if a%i == 0:
-        l1.append(i)
-
-for i in range(1, b+1):
-    if b%i == 0:
-        l2.append(i)
-
-common_list = list(set(l1) & set(l2))
-
-c = max(common_list)
-
-print(c)
-print(int(a*b/c))
+print(gcd(a, b))
+print(a*b // gcd(a, b))
