f309bace223b2bdf9b9c70d74356be734407d28a
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-05-29 15:37:00 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,10 @@
 import sys
 
-l = int(input())
+l = int(sys.stdin.readline())
 s = sys.stdin.readline()
 sum = 0
 
 for i in range(0, l):
     sum += (ord(s[i]) - 96) * (31 ** i)
     
-print(sum)
+print(sum % 1234567891)
