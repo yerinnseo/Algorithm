c0d21fc26a74a3eaaa69337aebbe1e1299bdd6c6
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-06-13 09:05:44 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+a = [0] * N
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for l in range(i - 1, j):
+        a[l] = k
+
+print(*a)
