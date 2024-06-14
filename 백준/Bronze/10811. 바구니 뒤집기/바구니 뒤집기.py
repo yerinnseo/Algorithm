2fffb256eaa7e11029aa682235e0bcd1d963c97b
yerinnseo <141564685+yerinnseo@users.noreply.github.com>
2024-06-14 17:32:38 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,18 +1,12 @@
-import sys
-
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
 
 a = [i for i in range(0, n+1)]
 
 for _ in range(m):
     i, j = map(int, input().split())
-    for _ in range((j - i + 1)//2):
-        temp = a[j]
-        a[j] = a[i]
-        a[i] = temp
-        j -= 1
-        i += 1
-
+    for k in range(0, (j - i + 1) // 2):
+        temp = a[j - k]
+        a[j - k] = a[i + k]
+        a[i + k] = temp
+        
 print(*a[1:])
